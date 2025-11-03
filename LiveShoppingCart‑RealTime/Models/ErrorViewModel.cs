f216ef7b12510f27a74156404ef03d0ae7e24ae7
Rohan R Raikar <rohanraikar7993@gmail.com>
2025-11-03 13:46:28 +0530

    Initial commit for LiveShoppingCart-RealTime

---
+++ LiveShoppingCart‑RealTime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiveShoppingCart_RealTime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
