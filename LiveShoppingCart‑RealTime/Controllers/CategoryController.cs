9499b2ca1d33396e2161f90b7eeb9b53caace456
Rohan R Raikar <rohanraikar7993@gmail.com>
2025-11-07 17:19:53 +0530

    Update CategoryController with Logger and Try-Catch Blocks

--- LiveShoppingCart‑RealTime/Controllers/CategoryController.cs
+++ LiveShoppingCart‑RealTime/Controllers/CategoryController.cs
@@ -8,7 +8,12 @@ namespace LiveShoppingCart_RealTime.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public CategoryController(ApplicationDbContext context) => _context = context;
+        private readonly ILogger<CategoryController> _logger;
+        public CategoryController(ApplicationDbContext context, Logger<CategoryController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
 
         [HttpGet]
         public async Task<IActionResult> Index() =>
@@ -21,58 +26,124 @@ namespace LiveShoppingCart_RealTime.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Category category)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(category);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(category);
+                    await _context.SaveChangesAsync();
+
+                    TempData["SweetAlertMessage"] = "Category Added Successfully!";
+                    TempData["SweetAlertType"] = "success";
+
+                    _logger.LogInformation("New Category Added Successfully");
+
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(category);
             }
-            return View(category);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception Occured while Creating Category");
+                return View(ex);
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null) return NotFound();
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null) return NotFound();
-            return View(category);
+            try
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+                var category = await _context.Categories.FindAsync(id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                return View(category);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,"Error Occured while Editing Category");
+                return View(ex);
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, Category category)
         {
-            if (id != category.Id) return NotFound();
-            if (ModelState.IsValid)
+            try
             {
-                _context.Update(category);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (id != category.Id) return NotFound();
+                if (ModelState.IsValid)
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+
+                    TempData["SweetAlertMessage"] = "Category Edited Successfully!";
+                    TempData["SweetAlertType"] = "success";
+
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(category);
             }
-            return View(category);
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Error Occured while Editing Category");
+                return View(ex);
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null) return NotFound();
-            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
-            if (category == null) return NotFound();
-            return View(category);
+            try
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+                var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                return View(category);
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Error Occured while Deleting Category");
+                return View(ex);
+            }
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var category = await _context.Categories.FindAsync(id);
-            if (category != null)
+            try
             {
-                _context.Categories.Remove(category);
-                await _context.SaveChangesAsync();
+                var category = await _context.Categories.FindAsync(id);
+                if (category != null)
+                {
+                    _context.Categories.Remove(category);
+                    await _context.SaveChangesAsync();
+                }
+
+                TempData["SweetAlertMessage"] = "Category Deleted Successfully!";
+                TempData["SweetAlertType"] = "success";
+
+                return RedirectToAction(nameof(Index));
             }
-            return RedirectToAction(nameof(Index));
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Error Occured while Deleting Category");
+                return View(ex);
+            }
         }
     }
 }
